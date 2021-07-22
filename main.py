e1c758c602ab1b5078fb9a5879b71d0ff45ea581
tocchimittsu <arimagedon21@gmail.com>
2021-07-22 15:44:32 +0900

    修正

---
+++ main.py
@@ -0,0 +1,38 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        # Windowの初期設定を行う。
+        super().__init__(master)
+
+        # Windowの画面サイズを設定する。
+        # geometryについて : https://kuroro.blog/python/rozH3S2CYE0a0nB3s2QL/
+        self.master.geometry("300x200")
+
+        # Windowを親要素として、frame Widget(Frame)を作成する。
+        # Frameについて : https://kuroro.blog/python/P20XOidA5nh583fYRvxf/
+        frame = tk.Frame(self.master)
+        # Windowを親要素とした場合に、frame Widget(Frame)をどのように配置するのか?
+        # packについて : https://kuroro.blog/python/UuvLfIBIEaw98BzBZ3FJ/
+        frame.pack()
+
+        # frame Widget(Frame)を親要素として、combobox Widgetを作成する。
+        # values : comboboxを選択すると、表示される様々な選択肢(バイキンマン, 食パンまんとする)の設定
+        combobox = ttk.Combobox(frame, values=('バイキンマン', '食パンまん'))
+
+        # frame Widget(Frame)を親要素とした場合に、combobox Widgetをどのように配置するのか?
+        # packについて : https://kuroro.blog/python/UuvLfIBIEaw98BzBZ3FJ/
+        combobox.pack()
+
+# Tkinter初学者参考 : https://docs.python.org/ja/3/library/tkinter.html#a-simple-hello-world-program
+if __name__ == "__main__":
+    # Windowを生成する。
+    # Windowについて : https://kuroro.blog/python/116yLvTkzH2AUJj8FHLx/
+    root = tk.Tk()
+    app = Application(master=root)
+
+    # Windowをループさせて、継続的にWindow表示させる。
+    # mainloopについて : https://kuroro.blog/python/DmJdUb50oAhmBteRa4fi/
+    app.mainloop()
